0e57069b07312bcca22969034f6a9f8480f05195
Adam Chen <adam.chen@managebac.com>
2020-12-06 15:43:55 +0800

    feature: Upload day3 part 2

---
+++ day3/part2/main.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func main() {
+	content, err := ioutil.ReadFile("input")
+	check(err)
+	lines_str := strings.Split(string(content), "\n")
+	forest_length := len(lines_str)
+	forest_width := len(lines_str[0])
+	steps := [][]int{{1, 1}, {3, 1}, {5, 1}, {7, 1}, {1, 2}}
+	for _, v := range steps {
+		right_step := v[0]
+		down_step := v[1]
+		trees_cnt := 0
+		for i, j := down_step, right_step; i < forest_length; i, j = i+down_step, j+right_step {
+			if string(lines_str[i][j%forest_width]) == "#" {
+				trees_cnt++
+			}
+		}
+		fmt.Println("Trees counts is: " + strconv.Itoa(trees_cnt))
+	}
+
+}
