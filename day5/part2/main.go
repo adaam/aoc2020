3d567a1bba4faae3bf448050ea0789e87b2a9987
Adam Chen <adam.chen@managebac.com>
2020-12-10 18:38:46 +0800

    feature: Upload day5 part 2

---
+++ day5/part2/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"io/ioutil"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func check(e error) {
+	if e != nil {
+		panic(e)
+	}
+}
+
+func replace(str string, replaceTo0 string, replaceTo1 string) string {
+	tmp1 := strings.ReplaceAll(str, replaceTo0, "0")
+	result := strings.ReplaceAll(tmp1, replaceTo1, "1")
+	return result
+}
+
+func main() {
+	content, err := ioutil.ReadFile("input")
+	check(err)
+	lines_str := strings.Split(string(content), "\n")
+	var seats []int
+
+	for _, v := range lines_str {
+		if len(v) <= 0 {
+			break
+		}
+		row_bin := replace(v[0:7], "F", "B")
+		col_bin := replace(v[7:10], "L", "R")
+		row, _ := strconv.ParseInt(row_bin, 2, 8)
+		col, _ := strconv.ParseInt(col_bin, 2, 4)
+
+		num := row*8 + col
+		seats = append(seats, int(num))
+
+	}
+	sort.Ints(seats)
+	for k, v := range seats {
+		if seats[k+1] != v+1 {
+			fmt.Println(v + 1)
+			break
+		}
+	}
+}
